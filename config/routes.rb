ad50e19f19302d5d3c4157b387472924855166d2
shogo.sato <ss.rk1993@gmail.com>
2020-02-15 08:26:49 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root to: 'categories#index'
   
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   get 'signup', to: 'users#new'
   resources :users, except: [:index, :destroy]
 end
